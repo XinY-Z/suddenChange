e36af445b9723adeafcfb71d568da99ee685493b
XinY-Z <animintegrius84260@gmail.com>
2025-05-07 21:38:16 -0600

    Cleaned and organized codes, added comments for readability

--- analysis.R
+++ analysis.R
@@ -23,18 +23,24 @@ dt[, max(messageInd), by=encounter_id][, quantile(V1, c(0.025, 0.975))]
 
 
 # prevalence ####
+
+## sudden gains
 dt.cpt[suddGains!='' & !is.na(suddGains), .N]/dt.cpt[, .N]
 dt.cpt[suddGains!='' & !is.na(suddGains), median(str_count(suddGains, ','))+1]
 dt.cpt[suddGains!='' & !is.na(suddGains), max(str_count(suddGains, ','))+1]
 
+## sudden losses
 dt.cpt[suddLosses!='' & !is.na(suddLosses), .N]/dt.cpt[, .N]
 dt.cpt[suddLosses!='' & !is.na(suddLosses), median(str_count(suddLosses, ','))+1]
 dt.cpt[suddLosses!='' & !is.na(suddLosses), max(str_count(suddLosses, ','))+1]
 
+## chats with gains and losses
 dt.cpt[suddGains!='' & !is.na(suddGains) & suddLosses!='' & !is.na(suddLosses), .N]
 
 
 # timing ####
+
+## sudden gains
 temp <- dt.cpt[
   suddGains!='' & !is.na(suddGains), 
   as.numeric(str_split(suddGains, ',', simplify=T))
@@ -44,6 +50,7 @@ median(temp, na.rm=T)
 sd(temp, na.rm=T)
 quantile(temp, c(0.025, 0.975), na.rm=T)
 
+## sudden losses
 temp <- dt.cpt[
   suddLosses!='' & !is.na(suddLosses), 
   as.numeric(str_split(suddLosses, ',', simplify=T))
@@ -54,35 +61,40 @@ sd(temp, na.rm=T)
 quantile(temp, c(0.025, 0.975), na.rm=T)
 
 
-# reversal ####
-gain_id <- dt.cpt[suddGains!='' & !is.na(suddGains), .(encounter_id, suddGains)]
-#gain_id <- dt.cpt[suddLosses!='' & !is.na(suddLosses), .(encounter_id, suddLosses)]
+# reversal rate ####
+
+## choose gains or losses to view
+VAR <- 'suddGains'
+if(VAR=='suddGains') {
+  sel <- dt.cpt[suddGains!='' & !is.na(suddGains), .(encounter_id, suddGains)]
+} else if(VAR=='suddLosses') {
+  sel <- dt.cpt[suddLosses!='' & !is.na(suddLosses), .(encounter_id, suddLosses)]
+}
 temp <- dt[
-  encounter_id %in% gain_id$encounter_id, 
+  encounter_id %in% sel$encounter_id, 
   .(encounter_id, messageInd, neural_prob)
 ]
 
 rev_encounters <- data.table()
-for(i in gain_id$encounter_id) {
-  
-  #if(i > 500) break
+for(i in sel$encounter_id) {
   
   cutoffs <- matrix(nrow=0, ncol=2)
   
-  gain_points <- gain_id[
+  sudd_chg <- sel[
     encounter_id==i, 
-    as.numeric(str_split(suddLosses, ',', simplify=T))
+    as.numeric(str_split(.SD, ',', simplify=T), .SDcols=VAR)
   ]
   
-  for(j in gain_points) {
+  ## find cutoff level of each sudden change at 50%
+  for(j in sudd_chg) {
     cutoff <- mean(c(
       temp[encounter_id==i & messageInd==j, neural_prob], 
       temp[encounter_id==i & messageInd==(j+1), neural_prob]
-    )
-    )
+    ))
     cutoffs <- rbind(cutoffs, c(j, cutoff))
   }
   
+  ## check if a session has reversal
   for(k in cutoffs[, 2]) {
     judge <- c()
     rev_points <- temp[encounter_id==i & neural_prob <= k, messageInd]
@@ -98,45 +110,38 @@ for(i in gain_id$encounter_id) {
   else rev_encounters <- rbind(rev_encounters, list(encounter_id=i, eval=F))
 }
 
+## number and proportion of sessions with reversal
+rev_encounters[, sum(eval)]
+rev_encounters[, mean(eval)]
 
-# magnitudes ####
+
+# magnitudes of sudden changes ####
+
 temp <- dt[, .(pre=mean(first(neural_prob, 3))), by=encounter_id]
 
-all_mag_gain <- c()
-for(i in gain_id$encounter_id) {
-  gain_points <- gain_id[
+all_mag <- c()
+for(i in sel$encounter_id) {
+  sudd_chg <- sel[
     encounter_id==i, 
-    as.numeric(str_split(suddGains, ',', simplify=T))
+    as.numeric(str_split(.SD, ',', simplify=T), .SDcols=VAR)
   ]
-  for(j in gain_points) {
+  for(j in sudd_chg) {
     mag <- dt[encounter_id==i & messageInd==(j+1), abs(absDiff)]
-    all_mag_gain <- c(all_mag_gain, mag)
+    all_mag <- c(all_mag, mag)
   }
 }
 
-all_mag_loss <- c()
-loss_id <- dt.cpt[suddLosses!='' & !is.na(suddLosses), .(encounter_id, suddLosses)]
-for(i in loss_id$encounter_id) {
-  loss_points <- loss_id[
-    encounter_id==i, 
-    as.numeric(str_split(suddLosses, ',', simplify=T))
-  ]
-  for(j in loss_points) {
-    mag <- dt[encounter_id==i & messageInd==(j+1), abs(absDiff)]
-    all_mag_loss <- c(all_mag_loss, mag)
-  }
-}
-
-mean(all_mag_gain/sd(temp$pre))
-mean(all_mag_loss/sd(temp$pre))
+## calculate magnitude relative to baseline SD
+mean(all_mag/sd(temp$pre))
 
 
-# both gains and losses ####
+# prevalence of chats with both gains and losses ####
 temp <- dt.cpt[suddGains!='' & !is.na(suddGains) & suddLosses!='' & !is.na(suddLosses)]
 temp[, .N]/dt.cpt[, .N]
 temp_gain <- temp[, as.numeric(str_split(suddGains, ',', simplify=T)), by=1:nrow(temp)]
 temp_loss <- temp[, as.numeric(str_split(suddLosses, ',', simplify=T)), by=1:nrow(temp)]
 
+## loss following gain
 gain_loss <- c()
 for(i in temp_gain[, unique(nrow)]) {
   gain_loss <- c(
@@ -147,6 +152,7 @@ for(i in temp_gain[, unique(nrow)]) {
 sum(gain_loss)
 mean(gain_loss)
 
+## gain following loss
 loss_gain <- c()
 for(i in temp_gain[, unique(nrow)]) {
   loss_gain <- c(
@@ -175,14 +181,15 @@ dt.cpt[, group := ifelse(
     'suddLoss',
     'No sudd'
   )
-)
-]
+)]
 dt.cpt[, group := factor(group, levels=c('suddGain', 'suddLoss', 'No sudd'))]
 
+## mean and sd
 dt.cpt[group=='suddGain', c(mean(chg_prob), sd(chg_prob))]
 dt.cpt[group=='suddLoss', c(mean(chg_prob), sd(chg_prob))]
 dt.cpt[group!='No sudd', c(mean(chg_prob), sd(chg_prob))]
 
+## anova
 kruskal.test(chg_prob ~ group, dt.cpt)
 pairwise.wilcox.test(
   dt.cpt$chg_prob, 
@@ -190,11 +197,12 @@ pairwise.wilcox.test(
   p.adjust.method='holm'
 )
 
+## effect size
 hedges_g(dt.cpt[group=='suddGain', chg_prob], dt.cpt[group=='suddLoss', chg_prob], pooled_sd=F)
 hedges_g(dt.cpt[group=='suddGain', chg_prob], dt.cpt[group=='No sudd', chg_prob], pooled_sd=F)
 hedges_g(dt.cpt[group=='suddLoss', chg_prob], dt.cpt[group=='No sudd', chg_prob], pooled_sd=F)
 
-# visualization
+## visualization
 ggplot(dt.cpt, aes(y=chg_prob, x=group)) +
   geom_hline(yintercept=0, linetype='solid', color='grey', size=0.8) +
   geom_violin(fill='#fbe9d2', color='#fbe9d2', alpha=0.7) +
